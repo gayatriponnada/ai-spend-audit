eb15bd232b7fd65afd2de8d2764facdac0ce57ae
gayatriponnada <gayatriponnada160@gmail.com>
2026-05-22 17:11:00 +0530

    small bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
         <Route path="/" element={<Home/>} />
-         <Route path="/" element={<AuditForm />} />
+         {/* <Route path="/" element={<AuditForm />} /> */}
         </Route>
       </Routes>
     </>
